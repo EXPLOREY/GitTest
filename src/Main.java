121e7f5f02d5740a9d5ebf0e7db98ca1e155e905
EXPLOREY <alihaj652379@gmail.com>
2025-02-27 18:49:37 +0300

    value updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a =0 ;
-        System.out.println(a);
+        int a = 0;
+        int b = 1;
+        System.out.println(a + b);
     }
 }
